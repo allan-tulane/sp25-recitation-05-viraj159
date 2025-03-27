51599b223d690f7dab389396e24ae830fa32585c
viraj159 <virajchoksi276@gmail.com>
2025-03-26 22:56:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,92 +3,79 @@ import tabulate
 
 def ssort(L):
     ### selection sort
-    if (len(L) == 1):
-        return(L)
+    if len(L) <= 1:
+        return L
     else:
         m = L.index(min(L))
-        # print('selecting minimum %s' % L[m])       
         L[0], L[m] = L[m], L[0]
-        # print('recursively sorting L=%s\n' % L[1:])
         return [L[0]] + ssort(L[1:])
-        
+
 def qsort(a, pivot_fn):
     if len(a) < 2:
         return a
-    # Select the pivot using the provided function:
     pivot_index = pivot_fn(a)
     pivot_value = a[pivot_index]
-    # Partition the list into elements less than, equal to, and greater than the pivot.
     left = [x for x in a if x < pivot_value]
     middle = [x for x in a if x == pivot_value]
     right = [x for x in a if x > pivot_value]
     return qsort(left, pivot_fn) + middle + qsort(right, pivot_fn)
 
-    
 def time_search(sort_fn, mylist):
     """
     Return the number of milliseconds to run this
     sort function on this list.
-
-    Note 1: `sort_fn` parameter is a function.
-    Note 2: time.time() returns the current time in seconds. 
-    You'll have to multiple by 1000 to get milliseconds.
-
-    Params:
-      sort_fn.....the search function
-      mylist......the list to search
-      key.........the search key 
-
-    Returns:
-      the number of milliseconds it takes to run this
-      search function on this input.
     """
     start = time.time()
     sort_fn(mylist)
     return (time.time() - start) * 1000
-    ###
 
-def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]):
+def compare_sort(sizes=[50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 30000]):
     """
     Compare the running time of different sorting algorithms.
-
-    Returns:
-      A list of tuples of the form
-      (n, linear_search_time, binary_search_time)
-      indicating the number of milliseconds it takes
-      for each method to run on each value of n
     """
-    ### TODO - sorting algorithms for comparison
-    # Define two variants of quicksort using currying:
     qsort_fixed_pivot = lambda lst: qsort(lst, lambda lst: 0)
     qsort_random_pivot = lambda lst: qsort(lst, lambda lst: random.randrange(len(lst)))
     tim_sort = lambda lst: sorted(lst)
-
     
-    result = []
+    results_random = []
+    results_sorted = []
+    
     for size in sizes:
-        # create list in ascending order
         mylist = list(range(size))
-        # shuffles list if needed
-        #random.shuffle(mylist)
-        result.append([
-            len(mylist),
+        random.shuffle(mylist)
+        results_random.append([
+            size,
+            time_search(ssort, mylist.copy()) if size <= 1000 else None,
             time_search(qsort_fixed_pivot, mylist.copy()),
             time_search(qsort_random_pivot, mylist.copy()),
             time_search(tim_sort, mylist.copy())
         ])
-    return result
-    ###
+        
+        mylist_sorted = list(range(size))
+        results_sorted.append([
+            size,
+            time_search(ssort, mylist_sorted.copy()) if size <= 1000 else None,
+            time_search(qsort_fixed_pivot, mylist_sorted.copy()),
+            time_search(qsort_random_pivot, mylist_sorted.copy()),
+            time_search(tim_sort, mylist_sorted.copy())
+        ])
+    
+    return results_random, results_sorted
 
-def print_results(results):
-    """ change as needed for comparisons """
-    print(tabulate.tabulate(results,
-                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot', 'timsort'],
-                            floatfmt=".3f",
-                            tablefmt="github"))
+def print_results(results, title):
+    """
+    Print formatted results using tabulate.
+    """
+    print(f"\n{title}")
+    print(tabulate.tabulate(results, headers=['n', 'ssort', 'qsort-fixed', 'qsort-random', 'timsort'], floatfmt=".3f", tablefmt="github"))
 
 def test_print():
-    print_results(compare_sort())
+    """
+    Run the sorting comparisons and print results.
+    """
+    results_random, results_sorted = compare_sort()
+    print_results(results_random, "Comparison on Random Permutations")
+    print_results(results_sorted, "Comparison on Sorted Lists")
 
 random.seed()
 test_print()
